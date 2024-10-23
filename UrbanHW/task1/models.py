8c14688850e24f910c10f55efb0c72e1523e6dea
Telegin Oleg <teleginos@mail.ru>
2024-10-23 14:26:34 +0300

    add HW_QuerySet.txt

--- UrbanHW/task1/models.py
+++ UrbanHW/task1/models.py
@@ -7,10 +7,16 @@ class Buyer(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=100, decimal_places=3)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='games')
+    buyer = models.ManyToManyField(Buyer, related_name='task1_game')
+
+    def __str__(self):
+        return self.title
